7dbb3ccb0d1afa1bcf79d594eb9b71fe30a1dad2
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2016-07-21 18:03:31 +0200

    Render detailed view with raw templates

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { VNode, div, h1, input, label, makeDOMDriver } from "@cycle/dom";
+import { VNode, a, div, h3, i, img, li, makeDOMDriver, nav, span, ul } from "@cycle/dom";
 import { DOMSource } from "@cycle/dom/xstream-typings";
 import { run } from "@cycle/xstream-run";
 import { Stream } from "xstream";
@@ -11,31 +11,142 @@ interface ISinks {
   DOM: Stream<VNode>;
 }
 
-function intent(DOM: DOMSource): Stream<string> {
-  return DOM.select(".field").events("input")
-    .map(ev => (ev.target as HTMLInputElement).value);
-}
-
-function model(name$: Stream<string>) {
-  return name$.startWith("");
-}
-
-function view(name$: Stream<string>) {
-  return name$.map(name =>
-    div(".container", [
-      label("Name: "),
-      input(".field", { attrs: { type: "text", value: name } }),
-      h1(name ? `Hello, ${name}!` : "Please enter your name..."),
+// A function that returns the hyperscript representation of the navigation.
+// Anytime the input stream emits an event, it will emit a new representation
+// in the output stream.
+function renderNav(input$: Stream<any>): Stream<VNode> {
+  return input$.map(() =>
+    nav(".bg-color-primary", [
+      div(".nav-wrapper", [
+        a({ "attrs": { "href": "list.html" } }, [
+          img(
+            ".logo",
+            {
+              "attrs": {
+                "src": "src/images/logo-people.svg",
+                "className": "logo",
+              },
+            }
+          ),
+        ]),
+        ul(".right.hide-on-med-and-down", [
+          li([
+            a({ "attrs": { "href": "list.html" } }, [`Peoples`]),
+          ]),
+        ]),
+      ]),
     ])
   );
 }
 
+function renderContainer(input$: Stream<any>): Stream<VNode> {
+  return input$.map(() =>
+    div(".container", [
+      div(".row", [
+        div(".col.s6.offset-s3", [
+          div(".card-panel", [
+            div(".row", [
+              div(".col.s7", [
+                div(".layout.vertical.flex", [
+                  a(
+                    ".username",
+                    { "attrs": { "src": "detail.html" } },
+                    [
+                      span("Bob "),
+                      span(".uppercase", "Dylan"),
+                    ],
+                  ),
+                ]),
+                div(".pad-top", [
+                  div([
+                    img({ "attrs": { "src": "src/images/md-email.svg" } }),
+                    span(" bobo@soc.com"),
+                  ]),
+                  div([
+                    img({ "attrs": { "src": "src/images/md-phone.svg" } }),
+                    span(" 0949494994"),
+                  ]),
+                ]),
+                div([
+                  div([
+                    span(".label", "Manager: "),
+                    span("Paul"),
+                  ]),
+                  div([
+                    span(".label", "Location: "),
+                    span("Paris"),
+                  ]),
+                ]),
+              ]),
+              div(".col.s5", [
+                img(
+                  ".picture",
+                  {
+                    "attrs": {
+                      "src": "https://randomuser.me/portraits/men/59.jpg",
+                    },
+                  },
+                ),
+                img(".icon", { "attrs": { "src": "src/images/md-map.svg" } }),
+                a(
+                  { "attrs": { "href": "edit.html" } },
+                  [i(".icon.material-icons", "mode_edit")],
+                ),
+                a(
+                  { "attrs": { "href": "list.html" } },
+                  [i(".icon.material-icons", "delete")],
+                ),
+              ]),
+            ]),
+            div(".skills", [
+              h3("Skills"),
+              a(".btn-large", "JS"),
+              a(".btn-large", "HTML5"),
+              a(".btn-large", "CSS3"),
+            ]),
+            div(".row.center-align", [
+              img(
+                ".pad-horizontal",
+                { "attrs": { "src": "src/images/md-twitter.svg" } }
+              ),
+              img(
+                ".pad-horizontal",
+                { "attrs": { "src": "src/images/md-slack.svg" } }
+              ),
+              img(
+                ".pad-horizontal",
+                { "attrs": { "src": "src/images/md-github.svg" } }
+              ),
+              img(
+                ".pad-horizontal",
+                { "attrs": { "src": "src/images/md-linkedin.svg" } }
+              ),
+            ]),
+          ]),
+        ]),
+      ]),
+    ])
+  );
+}
+
+// Our main application logic.
+// Takes observables input sources from drivers.
+// Does some pure dataflow operations = the app logic.
+// Returns observables output sinks to the drivers.
 function main(sources: ISources): ISinks {
   return {
-    DOM: view(model(intent(sources.DOM))),
+    // Combine all views into a single container to render within #app.
+    DOM: Stream.combine(
+      renderNav(Stream.of("")),
+      renderContainer(Stream.of(""))
+    ).map(div),
   };
 }
 
-run(main, {
+// Declare drivers that will perform the side-effects.
+const drivers = {
   DOM: makeDOMDriver("#app"),
-});
+};
+
+// Kick-off the application!
+run(main, drivers);
