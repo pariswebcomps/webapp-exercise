3cee6b67b2479d9d24c6d6d433282ed2c0ed6945
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2016-07-21 23:40:36 +0200

    Refactor Person component out of main app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-import { VNode, a, div, h3, i, img, li, makeDOMDriver, nav, span, ul } from "@cycle/dom";
+import Person from "./components/Person/Person";
+import { VNode, a, div, img, li, makeDOMDriver, nav, ul } from "@cycle/dom";
 import { DOMSource } from "@cycle/dom/xstream-typings";
 import { run } from "@cycle/xstream-run";
-// Use ramda instead of lodash/fp because it can't make the latter
-// to work with TypeScript easily.
-import { map, mapObjIndexed, pipe, values } from "ramda";
 import { Stream } from "xstream";
 
 interface ISources {
@@ -42,92 +40,12 @@ function renderNav(input$: Stream<any>): Stream<VNode> {
   );
 }
 
-function renderContainer(input$: Stream<any>): Stream<VNode> {
-  const renderLinks = pipe(
-    mapObjIndexed((link, type) =>
-      a(
-        { "attrs": { "href": link } },
-        [
-          img(
-            ".pad-horizontal",
-            { "attrs": { "src": `src/images/md-${type}.svg` } }
-          ),
-        ],
-      )
-    ),
-    values
-  );
-
-  const renderLargeBtns = map((text) => a(".btn-large", text));
-
-  return input$.map(({email, firstname, lastname, links, manager, phone, photo, skills}) =>
-    div(".container", [
-      div(".row", [
-        div(".col.s6.offset-s3", [
-          div(".card-panel", [
-            div(".row", [
-              div(".col.s7", [
-                div(".layout.vertical.flex", [
-                  a(
-                    ".username",
-                    { "attrs": { "src": "detail.html" } },
-                    [
-                      span(`${firstname} `),
-                      span(".uppercase", lastname),
-                    ],
-                  ),
-                ]),
-                div(".pad-top", [
-                  div([
-                    img({ "attrs": { "src": "src/images/md-email.svg" } }),
-                    span(` ${email}`),
-                  ]),
-                  div([
-                    img({ "attrs": { "src": "src/images/md-phone.svg" } }),
-                    span(` ${phone}`),
-                  ]),
-                ]),
-                div([
-                  div([
-                    span(".label", "Manager: "),
-                    span(manager),
-                  ]),
-                ]),
-              ]),
-              div(".col.s5", [
-                img(
-                  ".picture",
-                  { "attrs": { "src": photo } },
-                ),
-                img(".icon", { "attrs": { "src": "src/images/md-map.svg" } }),
-                a(
-                  { "attrs": { "href": "edit.html" } },
-                  [i(".icon.material-icons", "mode_edit")],
-                ),
-                a(
-                  { "attrs": { "href": "list.html" } },
-                  [i(".icon.material-icons", "delete")],
-                ),
-              ]),
-            ]),
-            div(".skills", [
-              h3("Skills"),
-              ...renderLargeBtns(skills),
-            ]),
-            div(".row.center-align", renderLinks(links)),
-          ]),
-        ]),
-      ]),
-    ])
-  );
-}
-
 // Our main application logic.
 // Takes observables input sources from drivers.
 // Does some pure dataflow operations = the app logic.
 // Returns observables output sinks to the drivers.
 function main(sources: ISources): ISinks {
-  const people$ = Stream.of({
+  const profile$ = Stream.of({
     "id": "5763cd4d9d2a4f259b53c901",
     "photo": "https://randomuser.me/portraits/women/59.jpg",
     "firstname": "Leanne",
@@ -153,11 +71,13 @@ function main(sources: ISources): ISinks {
     "managerId": "5763cd4d3b57c672861bfa1f",
   });
 
+  const person = Person({profile: profile$});
+
   return {
     // Combine all views into a single container to render within #app.
     DOM: Stream.combine(
       renderNav(Stream.of("")),
-      renderContainer(people$)
+      person.DOM
     ).map(div),
   };
 }
---
+++ src/components/Person/Person.ts
@@ -0,0 +1,98 @@
+import { VNode, a, div, h3, i, img, span } from "@cycle/dom";
+import { map, mapObjIndexed, pipe, values } from "ramda";
+import { Stream } from "xstream";
+
+interface ISources {
+  profile: Stream<any>;
+}
+
+interface ISinks {
+  DOM: Stream<VNode>;
+}
+
+const renderLinks = pipe(
+  mapObjIndexed((link, type) =>
+    a(
+      { "attrs": { "href": link } },
+      [
+        img(
+          ".pad-horizontal",
+          { "attrs": { "src": `src/images/md-${type}.svg` } }
+        ),
+      ],
+    )
+  ),
+  values
+);
+
+const renderLargeBtns = map((text) => a(".btn-large", text));
+
+function renderPerson(input$: Stream<any>): Stream<VNode> {
+  return input$.map(
+    ({email, firstname, lastname, links, manager, phone, photo, skills}) =>
+      div(".container", [
+        div(".row", [
+          div(".col.s6.offset-s3", [
+            div(".card-panel", [
+              div(".row", [
+                div(".col.s7", [
+                  div(".layout.vertical.flex", [
+                    a(
+                      ".username",
+                      { "attrs": { "src": "detail.html" } },
+                      [
+                        span(`${firstname} `),
+                        span(".uppercase", lastname),
+                      ],
+                    ),
+                  ]),
+                  div(".pad-top", [
+                    div([
+                      img({ "attrs": { "src": "src/images/md-email.svg" } }),
+                      span(` ${email}`),
+                    ]),
+                    div([
+                      img({ "attrs": { "src": "src/images/md-phone.svg" } }),
+                      span(` ${phone}`),
+                    ]),
+                  ]),
+                  div([
+                    div([
+                      span(".label", "Manager: "),
+                      span(manager),
+                    ]),
+                  ]),
+                ]),
+                div(".col.s5", [
+                  img(
+                    ".picture",
+                    { "attrs": { "src": photo } },
+                  ),
+                  img(".icon", { "attrs": { "src": "src/images/md-map.svg" } }),
+                  a(
+                    { "attrs": { "href": "edit.html" } },
+                    [i(".icon.material-icons", "mode_edit")],
+                  ),
+                  a(
+                    { "attrs": { "href": "list.html" } },
+                    [i(".icon.material-icons", "delete")],
+                  ),
+                ]),
+              ]),
+              div(".skills", [
+                h3("Skills"),
+                ...renderLargeBtns(skills),
+              ]),
+              div(".row.center-align", renderLinks(links)),
+            ]),
+          ]),
+        ]),
+      ])
+  );
+}
+
+export default function Person(sources: ISources): ISinks {
+  return {
+    DOM: renderPerson(sources.profile),
+  };
+};
