c314ba9e58049b27180d7a653f614de47bc5fcd4
Eduardo San Martin Morote <posva13@gmail.com>
2016-08-15 16:28:23 +0200

    Copy images

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <template>
-  <nav>
-    <div class="nav-wrapper">
-      <a href="list.html"><img class="logo" src="../original/images/logo-people.svg"></a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="list.html">Peoples</a></li>
-      </ul>
-    </div>
-  </nav>
+  <NavBar></NavBar>
 </template>
 
 <script>
+import NavBar from 'components/NavBar.vue'
+
 export default {
   data () {
     return {
       msg: 'Hello Vue 2.0!'
     }
+  },
+  components: {
+    NavBar
   }
 }
 </script>
 
 <style>
-.logo {
-  margin-left: 10px;
-  height: 100%;
-}
-
-nav {
-  background-color: #0168AB;
-}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="list.html"><img class="logo" src="../assets/images/logo-people.svg"></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="list.html">Peoples</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.logo {
+  margin-left: 10px;
+  height: 100%;
+}
+
+nav {
+  background-color: #0168AB;
+}
+</style>
