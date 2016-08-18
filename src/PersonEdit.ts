49a318ed86f96a2bc002d7386e93760a46e95138
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2016-08-18 13:47:40 +0200

    Render person edition form

---
+++ src/PersonEdit.ts
@@ -0,0 +1,96 @@
+import { VNode, a, div, form, input, label, span } from "@cycle/dom";
+import { HTTPSource, RequestInput } from "@cycle/http/src/interfaces";
+import { prop } from "ramda";
+import { Stream } from "xstream";
+
+interface IProfile {
+  id: String;
+  email: String;
+  firstname: String;
+  lastname: String;
+  phone: String;
+}
+
+interface IProps {
+  id: String;
+}
+
+interface ISources {
+  HTTP: HTTPSource;
+  props: Stream<IProps>;
+}
+
+interface ISinks {
+  DOM: Stream<VNode>;
+  HTTP: Stream<RequestInput>;
+}
+
+const renderCardAction = () =>
+  div(".card-action", [
+    // TODO: implement cancel (back to history)
+    a({ "attrs": { "href": "/" } }, "Cancel"),
+    // TODO: implement submit
+    a({ "attrs": { "href": "/" } }, "Submit"),
+  ]);
+
+const renderCardContent = ({firstname, lastname, email, phone}) =>
+  div(".card-content", [
+    span(".card-title", "Contact information"),
+    form([
+      div(".row", [
+        div(".input-field.col.s12", [
+          input("#title", { "attrs": { "type": "text", "value": firstname } }),
+          label({ "attrs": {"for": "title"}}, "First name"),
+        ]),
+      ]),
+      div(".row", [
+        div(".input-field.col.s12", [
+          input("#last-name", { "attrs": { "type": "text", "value": lastname } }),
+          label({ "attrs": {"for": "last-name", "data-error": "wrong", "data-success": "right"}}, "Last name"),
+        ]),
+      ]),
+      div(".row", [
+        div(".input-field.col.s12", [
+          input("#email", { "attrs": { "type": "text", "value": email } }),
+          label({ "attrs": {"for": "email"}}, "Email"),
+        ]),
+      ]),
+      div(".row", [
+        div(".input-field.col.s12", [
+          input("#phone", { "attrs": { "type": "text", "value": phone } }),
+          label({ "attrs": {"for": "phone"}}, "Phone number"),
+        ]),
+      ]),
+    ]),
+  ]);
+
+function renderForm(
+  profile$: Stream<IProfile>
+): Stream<VNode> {
+  return profile$.map((profile) =>
+    div(".container", [
+      div(".row.movieCardForm", [
+        div(".col.s12.card.z-depth-3", [
+          renderCardContent(profile),
+          renderCardAction(),
+        ]),
+      ]),
+    ])
+  );
+}
+
+export default function PersonDetail({HTTP, props}: ISources): ISinks {
+  const profile$ = HTTP.select("person-edit").flatten().map(prop("body"));
+
+  // Fetch the API for person profile.
+  const personsRequest$ = props
+    .map(({id}) => ({
+      category: "person-edit",
+      url: `http://localhost:3001/api/peoples/${id}`,
+    }));
+
+  return {
+    DOM: renderForm(profile$),
+    HTTP: personsRequest$,
+  };
+}
