094369d6e1f2cea2d196535f95da339ffe61d1a9
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2016-08-20 21:23:35 +0200

    Prevent default behavior on form submission

--- src/PersonEdit.ts
+++ src/PersonEdit.ts
@@ -1,6 +1,6 @@
 import { IProfile } from "./interfaces";
 
-import { VNode, a, div, form, input, label, span } from "@cycle/dom";
+import { VNode, a, button, div, form, input, label, span } from "@cycle/dom";
 import { DOMSource } from "@cycle/dom/xstream-typings";
 import { HTTPSource, RequestInput } from "@cycle/http/src/interfaces";
 import { prop } from "ramda";
@@ -20,44 +20,48 @@ interface ISources {
 interface ISinks {
   DOM: Stream<VNode>;
   HTTP: Stream<RequestInput>;
+  preventDefault: Stream<any>;
   router: any;
 }
 
 const renderCardAction = () =>
   div(".card-action", [
-    // Handle navigation with router driver to go back to previous page.
-    a(".cancel", { "attrs": { "href": "#" } }, "Cancel"),
-    // TODO: implement submit
-    a({ "attrs": { "href": "/" } }, "Submit"),
+    a(
+      ".cancel.btn.waves-effect.waves-light.deep-orange.m-right",
+      "Cancel"
+    ),
+    button(
+      ".btn.waves-effect.waves-light",
+      { "attrs": { "type": "submit", "name": "action" } },
+      "Submit"
+    ),
   ]);
 
 const renderCardContent = ({firstname, lastname, email, phone}) =>
   div(".card-content", [
     span(".card-title", "Contact information"),
-    form([
-      div(".row", [
-        div(".input-field.col.s12", [
-          input("#title.validate", { "attrs": { "type": "text", "value": firstname } }),
-          label(".active", { "attrs": { "for": "title" } }, "First name"),
-        ]),
+    div(".row", [
+      div(".input-field.col.s12", [
+        input("#title.validate", { "attrs": { "type": "text", "value": firstname } }),
+        label(".active", { "attrs": { "for": "title" } }, "First name"),
       ]),
-      div(".row", [
-        div(".input-field.col.s12", [
-          input("#last-name.validate", { "attrs": { "type": "text", "value": lastname } }),
-          label(".active", { "attrs": { "for": "last-name" } }, "Last name"),
-        ]),
+    ]),
+    div(".row", [
+      div(".input-field.col.s12", [
+        input("#last-name.validate", { "attrs": { "type": "text", "value": lastname } }),
+        label(".active", { "attrs": { "for": "last-name" } }, "Last name"),
       ]),
-      div(".row", [
-        div(".input-field.col.s12", [
-          input("#email.validate", { "attrs": { "type": "text", "value": email } }),
-          label(".active", { "attrs": { "for": "email" } }, "Email"),
-        ]),
+    ]),
+    div(".row", [
+      div(".input-field.col.s12", [
+        input("#email.validate", { "attrs": { "type": "text", "value": email } }),
+        label(".active", { "attrs": { "for": "email" } }, "Email"),
       ]),
-      div(".row", [
-        div(".input-field.col.s12", [
-          input("#phone.validate", { "attrs": { "type": "text", "value": phone } }),
-          label(".active", { "attrs": { "for": "phone" } }, "Phone number"),
-        ]),
+    ]),
+    div(".row", [
+      div(".input-field.col.s12", [
+        input("#phone.validate", { "attrs": { "type": "text", "value": phone } }),
+        label(".active", { "attrs": { "for": "phone" } }, "Phone number"),
       ]),
     ]),
   ]);
@@ -68,10 +72,14 @@ function renderForm(
   return profile$.map((profile) =>
     div(".container", [
       div(".row.movieCardForm", [
-        div(".col.s12.card.z-depth-3", [
-          renderCardContent(profile),
-          renderCardAction(),
-        ]),
+        form(
+          ".form.card.col.s12.z-depth-3",
+          { "attrs": { "action": `/api/peoples/${profile.id}` } },
+          [
+            renderCardContent(profile),
+            renderCardAction(),
+          ]
+        ),
       ]),
     ])
   );
@@ -80,6 +88,7 @@ function renderForm(
 export default function PersonEdit({DOM, HTTP, props}: ISources): ISinks {
   const profile$ = HTTP.select("person-edit").flatten().map(prop("body"));
   const cancelClick$ = DOM.select(".cancel").events("click");
+  const formSubmit$ = DOM.select(".form").events("submit");
 
   // Fetch the API for person profile.
   const personsRequest$ = props.map(({apiUrl, id}) =>
@@ -88,7 +97,8 @@ export default function PersonEdit({DOM, HTTP, props}: ISources): ISinks {
   return {
     DOM: renderForm(profile$),
     HTTP: personsRequest$,
+    preventDefault: formSubmit$,
     // Every time we click on cancel, go back in history.
-    router: cancelClick$.mapTo(({type: "goBack"})),
+    router: cancelClick$.mapTo(({ type: "goBack" })),
   };
 }
