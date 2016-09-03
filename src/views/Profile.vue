c02d4b5e29380293a8cd61fa70514541d2a559ae
Eduardo San Martin Morote <posva13@gmail.com>
2016-09-03 19:11:39 +0200

    Add style and container to profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    <CardPanel v-if="person" :person="person" full="full"></CardPanel>
-    <p v-else>
-      Loading
-    </p>
+  <div class="container">
+    <div class="row">
+      <div class="col s6 offset-s3">
+        <CardPanel v-if="person" :person="person" full="full"></CardPanel>
+        <p v-else>
+          Loading
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,5 +31,29 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.skills {
+  padding: 10px;
+  background-color: #FAFAFA;
+}
+
+.skills h3 {
+  font-size: 24px;
+  font-weight: normal;
+  line-height: 1.1;
+}
+
+.skills a {
+  background-color: white;
+  color: #000;
+  margin: 5px;
+}
+
+.links{
+  text-align: center;
+}
+
+.links img{
+  padding:0 5px;
+}
 </style>
