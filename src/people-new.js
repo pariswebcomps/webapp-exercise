a25ff0a481f5a7d1d957916bcffc3b0540cf1f47
Ahmed RADJDI <ahmed.radjdi@gmail.com>
2016-09-04 08:37:13 -0400

    Add static behaviours

--- src/people-new.js
+++ src/people-new.js
@@ -1,3 +1,21 @@
+import environment from './environment';
+import {LogManager, inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+
+let logger = LogManager.getLogger('people-new');
+
+@inject(Router)
 export class PeopleNew {
-  message = 'Create a People'
+  constructor(router) {
+    this.router = router;
+  }
+  contact = {"id": "5763cd4d9d2a4f259b53c901", "photo": "https://randomuser.me/portraits/women/59.jpg", "firstname": "Leanne", "lastname": "Woodard", "entity": "BIOSPAN", "email": "Leanne.Woodard@BIOSPAN.com", "skills": ["pariatur", "ipsum", "laboris", "nostrud", "elit"], "phone": "0784112248", "links": { "twitter": "https://twitter.com/laboris", "slack": "https://slack.com/fugiat", "github": "https://github.com/velit", "linkedin": "https://www.linkedin.com/in/voluptate"}, "isManager": false, "manager": "Erika", "managerId": "5763cd4d3b57c672861bfa1f"};
+
+  activate(params, routeConfig, navigationInstruction) {
+    if (environment.debug) logger.info(`edit contact ${params.email}`);
+  }
+
+  save() {
+    this.router.navigateToRoute('people-show', { email: this.contact.email })
+  }
 }
--- src/people-show.js
+++ src/people-show.js
@@ -1,3 +1,12 @@
+import environment from './environment';
+import {LogManager} from 'aurelia-framework';
+
+let logger = LogManager.getLogger('people-show');
+
 export class PeopleShow {
-  message = 'Show a People'
+  contact = {"id": "5763cd4d9d2a4f259b53c901", "photo": "https://randomuser.me/portraits/women/59.jpg", "firstname": "Leanne", "lastname": "Woodard", "entity": "BIOSPAN", "email": "Leanne.Woodard@BIOSPAN.com", "skills": ["pariatur", "ipsum", "laboris", "nostrud", "elit"], "phone": "0784112248", "links": { "twitter": "https://twitter.com/laboris", "slack": "https://slack.com/fugiat", "github": "https://github.com/velit", "linkedin": "https://www.linkedin.com/in/voluptate"}, "isManager": false, "manager": "Erika", "managerId": "5763cd4d3b57c672861bfa1f"};
+
+  activate(params, routeConfig, navigationInstruction) {
+    if (environment.debug) logger.info(`navigate to ${params.email}`);
+  }
 }
