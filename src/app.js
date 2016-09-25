56c3e6c77f2d646a01938ab6a626b2cdda612d26
Ahmed RADJDI <ahmed.radjdi@gmail.com>
2016-09-25 14:50:01 +0200

    clean new view because we don't use it

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ export class App {
     config.title = 'Peoples';
     config.map([
       {route: ['', 'people'],       moduleId: 'people-list', name: 'people-list'},
-      {route: 'people/new',         moduleId: 'people-new',  name: 'people-new'},
       {route: 'people/:id',      moduleId: 'people-show', name: 'people-show'},
       {route: 'people/:id/edit', moduleId: 'people-edit', name: 'people-edit'}
     ]);
