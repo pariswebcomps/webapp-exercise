f963d00a90cdb1f1d1f91dc76e4a6f7450226b13
Ahmed RADJDI <ahmed.radjdi@gmail.com>
2016-09-25 12:47:18 +0200

    use 'id' to route parameters instead 'email'

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ export class App {
     config.map([
       {route: ['', 'people'],       moduleId: 'people-list', name: 'people-list'},
       {route: 'people/new',         moduleId: 'people-new',  name: 'people-new'},
-      {route: 'people/:email',      moduleId: 'people-show', name: 'people-show'},
-      {route: 'people/:email/edit', moduleId: 'people-edit', name: 'people-edit'}
+      {route: 'people/:id',      moduleId: 'people-show', name: 'people-show'},
+      {route: 'people/:id/edit', moduleId: 'people-edit', name: 'people-edit'}
     ]);
 
     this.router = router;
--- src/people-show.js
+++ src/people-show.js
@@ -7,6 +7,6 @@ export class PeopleShow {
   contact = {"id": "5763cd4d9d2a4f259b53c901", "photo": "https://randomuser.me/portraits/women/59.jpg", "firstname": "Leanne", "lastname": "Woodard", "entity": "BIOSPAN", "email": "Leanne.Woodard@BIOSPAN.com", "skills": ["pariatur", "ipsum", "laboris", "nostrud", "elit"], "phone": "0784112248", "links": { "twitter": "https://twitter.com/laboris", "slack": "https://slack.com/fugiat", "github": "https://github.com/velit", "linkedin": "https://www.linkedin.com/in/voluptate"}, "isManager": false, "manager": "Erika", "managerId": "5763cd4d3b57c672861bfa1f"};
 
   activate(params, routeConfig, navigationInstruction) {
-    if (environment.debug) logger.info(`navigate to ${params.email}`);
+    if (environment.debug) logger.info(`navigate to ${params.id}`);
   }
 }
