e1337142601c1c0e54412f4b4530822a826a0195
Ahmed RADJDI <ahmed.radjdi@gmail.com>
2016-09-25 15:06:46 +0200

    format code

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ export class App {
   configureRouter(config: RouterConfiguration, router: Router){
     config.title = 'Peoples';
     config.map([
-      {route: ['', 'people'],       moduleId: 'people-list', name: 'people-list'},
+      {route: ['', 'people'],    moduleId: 'people-list', name: 'people-list'},
       {route: 'people/:id',      moduleId: 'people-show', name: 'people-show'},
       {route: 'people/:id/edit', moduleId: 'people-edit', name: 'people-edit'}
     ]);
