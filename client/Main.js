3622aaba6db0d459e3f444ae3cb279a8f0a094ac
ThomasCrevoisier <crevoisier.thomas@gmail.com>
2016-10-03 12:43:02 +0200

    Bootstrap React stack

---
+++ client/Main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import React from "react";
+import { render } from "react-dom";
+
+import Nav from "./nav/Nav.js";
+
+render(<Nav />, document.getElementById('app'));
---
+++ client/nav/Nav.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import React from "react";
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <nav className="light-blue darken-4">
+        <div className="nav-wrapper">
+          <a href="blabla.html">
+            <img className="logo" src="static/images/logo-people.svg" />
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
