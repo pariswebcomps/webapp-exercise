c4f47584610fa7db966eb1cab2fb85ae36c36d13
Thomas Crevoisier <crevoisier.thomas@gmail.com>
2016-10-15 14:30:38 +0200

    Fix some style on the search bar

--- client/components/search/SearchBar.js
+++ client/components/search/SearchBar.js
@@ -8,8 +8,7 @@ class SearchBar extends React.Component {
       <form className="col s12 container">
         <div className="input-field">
           <i className="material-icons prefix">search</i>
-          <input id="search" type="text" />
-          <label className="" for="search">Search</label>
+          <input id="search" type="text" placeholder="Search some user" />
         </div>
       </form>
     );
