73f966c5d074efe389994ec8e05e28ca0fdc86be
Ahmed RADJDI <ahmed.radjdi@gmail.com>
2016-10-18 23:03:39 +0200

    Materialize and Validate edit form

--- src/people-edit.js
+++ src/people-edit.js
@@ -1,29 +1,33 @@
-import {
-  inject,
-  LogManager
-} from 'aurelia-framework';
+import {inject, NewInstance} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
+
+import {ValidationController, validateTrigger} from 'aurelia-validation';
+import {MaterializeFormValidationRenderer} from 'aurelia-materialize-bridge';
+
+import {Contact} from 'model/contact';
 import {PeopleService} from 'services/people';
+import {formalize} from 'decorators/forms';
 
-
-@inject(
-  PeopleService,
-  Router
-)
+@formalize
+@inject(NewInstance.of(ValidationController), PeopleService, Router)
 export class PeopleEdit {
-  constructor(peopleService, router) {
+  validationController = null;
+  dirtyContact = null;
+
+  constructor(validationController, peopleService, router) {
     this.peopleService = peopleService;
     this.router = router;
-  }
 
-  initialConcact = {};
-  dirtyContact = {};
+    this.validationController = validationController;
+    this.validationController.validateTrigger = validateTrigger.change;
+    this.validationController.addRenderer(new MaterializeFormValidationRenderer());
+  }
 
   activate(params, routeConfig, navigationInstruction) {
     return this.peopleService.get(params.id)
       .then(contact => {
-        this.initialConcact = contact;
-        this.dirtyContact = contact;
+        this.dirtyContact = new Contact(contact);
+        return null;
       });
   }
 
