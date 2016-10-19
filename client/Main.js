462f195f09b1758e3df20d5963a6e80eca193ab9
ThomasCrevoisier <crevoisier.thomas@gmail.com>
2016-10-19 13:36:38 +0200

    Allow the use of redux tool via chrome extension

--- client/Main.js
+++ client/Main.js
@@ -5,7 +5,7 @@ import { render } from "react-dom";
 
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import { syncHistoryWithStore, routerReducer } from "react-router-redux";
@@ -17,11 +17,13 @@ import Users from "./pages/Users.js";
 import UserDetail from "./pages/UserDetail.js";
 import UserForm from "./components/user/UserForm.js";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   combineReducers(Object.assign({}, reducers, {
     routing: routerReducer
   })),
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 const history = syncHistoryWithStore(hashHistory, store);
